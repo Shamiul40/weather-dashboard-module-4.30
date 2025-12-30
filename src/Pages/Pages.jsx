99cdb09f2b6bb5e144a59cf9fc013dc7cf36e7e6
Shamiul40 <shamiul4040@gmail.com>
2025-12-30 21:11:06 +0600

    weather board added

--- src/Pages/Pages.jsx
+++ src/Pages/Pages.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Header from '../Components/Header/Header'
+import Weather from '../Components/Wether/Weather'
 
 export default function Pages() {
   return (
     <>
      <Header /> 
+     <main>
+        <section>
+            <Weather />
+        </section>
+     </main>
     </>
   )
 }
