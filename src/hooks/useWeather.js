fd5ea4b2fbe05a894b162cb56a5bd347521f756c
Shamiul40 <shamiul4040@gmail.com>
2026-01-01 21:14:55 +0600

    return wether data and set error and loading

--- src/hooks/useWeather.js
+++ src/hooks/useWeather.js
@@ -24,16 +24,36 @@ const useWeather = () => {
 
   const fetchWeatherData = async (longitude, latitude) => {
     try {
+
+      setLoading({
+        ...loading,
+        state : true,
+        message: "data fetching......"
+      })
+
       const response = await fetch(
         `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid={API key}`
       );
       if(!response.ok) {
-        const message = `"data does not found ${response.status}`
+        const message = `"data does not found propersrly"${response.status}`
         throw new Error(message)
       }
+      const data = response.json();
+      setWeatherData(data)
+      
 
-    } catch {
+    } catch(error) {
+      setError(error)
     } finally {
+      setLoading({
+        ...loading,
+        state: false,
+        message: ""
+
+      })
+
     }
+
+    return weatherData
   };
 };
