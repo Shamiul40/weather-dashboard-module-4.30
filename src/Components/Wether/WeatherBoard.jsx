9a533987de5b5589992eccce6a8207c3a3a18349
Shamiul40 <shamiul4040@gmail.com>
2026-01-02 16:28:17 +0600

    loading

--- src/Components/Wether/WeatherBoard.jsx
+++ src/Components/Wether/WeatherBoard.jsx
@@ -1,27 +1,31 @@
-import React, { useContext } from 'react'
-import AddToFavourite from './AddToFavourite'
-import WeatherHeadline from './WeatherHeadline'
-import WeatherConditions from './WeatherConditions'
-import { WeatherContext } from '../../Context'
+import React, { useContext } from "react";
+import AddToFavourite from "./AddToFavourite";
+import WeatherHeadline from "./WeatherHeadline";
+import WeatherConditions from "./WeatherConditions";
+import { WeatherContext } from "../../Context";
 
 export default function WeatherBoard() {
-	const {weatherData, loading} =useContext(WeatherContext)
-	
+  const { weatherData, loading } = useContext(WeatherContext);
+
   return (
-   <section className="">
-			<div className="container">
-				<div
-					className="grid bg-black/20 rounded-xl backdrop-blur-md border-2 lg:border-[3px] border-white/[14%] px-4 lg:px-14 py-6 lg:py-10 min-h-[300px] max-w-[820px] mx-auto">
-					<div className="grid md:grid-cols-2 gap-10 md:gap-6">
-						<div className="md:col-span-2">
-							<AddToFavourite />
-						</div>
-						<WeatherHeadline />
-						<WeatherConditions />
-					</div>
-				</div>
-				
-			</div>
-		</section>
-  )
+    <section className="">
+      <div className="container">
+        <div className="grid bg-black/20 rounded-xl backdrop-blur-md border-2 lg:border-[3px] border-white/[14%] px-4 lg:px-14 py-6 lg:py-10 min-h-[300px] max-w-[820px] mx-auto">
+          {loading ? (
+            <p>loading...</p>
+          ) : (
+            <>
+              <div className="grid md:grid-cols-2 gap-10 md:gap-6">
+                <div className="md:col-span-2">
+                  <AddToFavourite />
+                </div>
+                <WeatherHeadline />
+                <WeatherConditions />
+              </div>
+            </>
+          )}
+        </div>
+      </div>
+    </section>
+  );
 }
