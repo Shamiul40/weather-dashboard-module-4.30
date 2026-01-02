f632cc47065057b3a81f20f526d9cd6417b160d3
Shamiul40 <shamiul4040@gmail.com>
2026-01-02 16:48:19 +0600

    data pass from server to weatherheadline

--- src/Components/Wether/WeatherBoard.jsx
+++ src/Components/Wether/WeatherBoard.jsx
@@ -11,8 +11,8 @@ export default function WeatherBoard() {
     <section className="">
       <div className="container">
         <div className="grid bg-black/20 rounded-xl backdrop-blur-md border-2 lg:border-[3px] border-white/[14%] px-4 lg:px-14 py-6 lg:py-10 min-h-[300px] max-w-[820px] mx-auto">
-          {loading ? (
-            <p>loading...</p>
+          {loading.state ? (
+            <p>{loading.message}</p>
           ) : (
             <>
               <div className="grid md:grid-cols-2 gap-10 md:gap-6">
--- src/Components/Wether/WeatherHeadline.jsx
+++ src/Components/Wether/WeatherHeadline.jsx
@@ -1,18 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import pinIcon from "../../assets/pin.svg"
+import { WeatherContext } from "../../Context";
 
 export default function WeatherHeadline() {
+
+  const {weatherData} = useContext(WeatherContext)
+  const {location, climate, temperature, time} = weatherData
   return (
     <div>
       <div class="max-md:flex items-center justify-between md:-mt-10">
         <img src="./assets/cloud.svg" alt="cloud" />
         <div class="max-md:flex items-center max-md:space-x-4">
           <h1 class="text-[60px] lg:text-[80px] xl:text-[100px] leading-none md:mb-4">
-            16°
+            {Math.round(temperature)}°
           </h1>
           <div class="flex items-center space-x-4 md:mb-4">
             <img src={pinIcon} />
-            <h2 class="text-2xl lg:text-[50px]">Dhaka</h2>
+            <h2 class="text-2xl lg:text-[50px]">{location}</h2>
           </div>
         </div>
       </div>
