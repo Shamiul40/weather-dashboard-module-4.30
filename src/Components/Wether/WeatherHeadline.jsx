efffd88bf648a068c8f9a9ce65de3dc616b196e8
Shamiul40 <shamiul4040@gmail.com>
2026-01-02 17:31:18 +0600

    climate icon use

--- src/Components/Wether/WeatherHeadline.jsx
+++ src/Components/Wether/WeatherHeadline.jsx
@@ -3,13 +3,35 @@ import pinIcon from "../../assets/pin.svg";
 import { WeatherContext } from "../../Context";
 import { getDateTime } from "../../utils/date";
 
+import cloudIcon from "../../assets/cloud.svg";
+import rainIcon from "../../assets/rainy.svg";
+import sunIcon from "../../assets/sun.svg";
+import thunderIcon from "../../assets/thunder.svg";
+import hazeIcon from "../../assets/haze.svg";
+
 export default function WeatherHeadline() {
   const { weatherData } = useContext(WeatherContext);
   const { location, climate, temperature, time } = weatherData;
+
+  const getClimateIcon = (climate) => {
+    switch (climate) {
+      case "cloud":
+        return cloudIcon;
+      case "rain":
+        return rainIcon;
+      case "thunder":
+        return thunderIcon;
+      case "haze":
+        return hazeIcon;
+      default:
+        return sunIcon;
+    }
+  };
+
   return (
     <div>
       <div class="max-md:flex items-center justify-between md:-mt-10">
-        <img src="./assets/cloud.svg" alt="cloud" />
+        <img src={getClimateIcon(climate)} alt="climate" />
         <div class="max-md:flex items-center max-md:space-x-4">
           <h1 class="text-[60px] lg:text-[80px] xl:text-[100px] leading-none md:mb-4">
             {Math.round(temperature)}°
@@ -20,7 +42,9 @@ export default function WeatherHeadline() {
           </div>
         </div>
       </div>
-      <p class="text-sm lg:text-lg">{getDateTime(time, "time", false)} - {getDateTime(time, "date", false)}</p>
+      <p class="text-sm lg:text-lg">
+        {getDateTime(time, "time", false)} - {getDateTime(time, "date", false)}
+      </p>
     </div>
   );
 }
