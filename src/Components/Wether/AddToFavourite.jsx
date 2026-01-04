dad8336177b60609466a8baeb1cbcef757154dc7
Shamiul40 <shamiul4040@gmail.com>
2026-01-04 23:55:05 +0600

    toggle icon

--- src/Components/Wether/AddToFavourite.jsx
+++ src/Components/Wether/AddToFavourite.jsx
@@ -1,9 +1,13 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import heart from "../../assets/heart.svg"
-import { FavouritesContext } from "../../Context";
+import redHeart  from "../../assets/heart-red.svg"
+import { FavouritesContext, WeatherContext } from "../../Context";
 
 export default function AddToFavourite() {
-  const {favourites, addFavourites, removeFavourites} =useContext(FavouritesContext)
+  const [isFavourite, setIsFavourite] = useState(false)
+  const {favourites, addFavourites, removeFavourites} =useContext(FavouritesContext);
+  const {weatherData} = useContext(WeatherContext);
+  const{latitude, longitude, location} = weatherData
   return (
     <div class="flex items-center justify-end space-x-6">
       <button class="text-sm md:text-base inline-flex items-center space-x-2 px-3 py-1.5 rounded-md bg-[#C5C5C54D]">
