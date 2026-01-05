3afa8ec6f61f1c15cf580195a168ae66a0eb686b
Shamiul40 <shamiul4040@gmail.com>
2026-01-05 22:17:45 +0600

    mock data make and export

--- src/Components/Header/Modal.jsx
+++ src/Components/Header/Modal.jsx
@@ -3,22 +3,21 @@ import { FavouritesContext } from "../../Context";
 import Favourites from "./Favourites";
 
 export default function Modal() {
-  const {favourites}=useContext(FavouritesContext)
+  const { favourites } = useContext(FavouritesContext);
 
   return (
     <div className="max-w-xs py-4 bg-white rounded-md border-gray-500 absolute right-0 top-16 text-black shadow-lg ">
       <h3 className="text-lg font-bold px-4">Favourite Locations</h3>
       <ul className="space-y-2 mt-4 *:py-2 *:px-4 *:cursor-pointer">
-       
-       {
-        Favourites.length > 0 ? (
-          favourites.map(fav=>(
-             <li key={fav.location} className="hover:bg-gray-200">{fav.location}</li>
+        {Favourites.length > 0 ? (
+          favourites.map((fav) => (
+            <li key={fav.location} className="hover:bg-gray-200">
+              {fav.location}
+            </li>
           ))
-        ) : (<p>favourites location is not added</p>)
-       }
-
-    
+        ) : (
+          <p> Nothing is added to favourites</p>
+        )}
       </ul>
     </div>
   );
